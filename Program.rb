de6d21b0a58a5a9a1552351e2287b6310f5088cc
domenicode <domenico@christophervigliotti.com>
2016-09-11 15:37:55 -0400

    My first version of HelloJoe

---
+++ Program.rb
@@ -0,0 +1,17 @@
+x=1
+while x == 1
+puts 'Enter One of these:'
+puts 'WhatsYourName'
+puts 'HowAreYou'
+command = gets.chomp
+if command == 'WhatsYourName'
+puts 'My Name is Joe'
+elsif command == 'HowAreYou'
+puts '| |'
+puts '_  _'
+puts ' -- '
+end
+end
+
+
+
