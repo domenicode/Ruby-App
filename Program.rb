8e392d209ac14372671793bcf61dad025cd84b6e
domenicode <domenico@christophervigliotti.com>
2016-09-17 07:43:38 -0400

    Changed Back to Good Old Joe, the other was just too much
    i added an exit command

--- Program.rb
+++ Program.rb
@@ -1,6 +1,6 @@
 x=1
-while x == 1
 puts 'use Help to see a list of commands'
+while x == 1
 command = gets.chomp
 if command == 'WhatsYourName'
 puts 'My Name is Jo (Josephine)'
@@ -12,21 +12,16 @@ elsif command == 'WhatDayIsIt'
 puts 'Today is ' + Time.now.strftime("%A")
 elsif command == 'WhatTimeIsIt'
 puts 'It is ' + Time.now.strftime("%I : %M %p %Z")
-elsif command == 'AreYouSingle'
-puts 'Yes'
-elsif command == 'WillYouMarryMe'
-puts 'Yes'
-elsif command == 'DoYouWantToGetMarriedSomeday'
-puts 'I do'
+elsif command == 'Exit'
+output= x=2 
 elsif command == 'Help'
 puts 'Enter One of these:'
 puts 'WhatsYourName'
 puts 'HowAreYou'
 puts 'WhatDayIsIt'
 puts 'WhatTimeIsIt'
-puts 'AreYouSingle'
-puts 'WillYouMarryMe'
-puts 'DoYouWantToGetMarriedSomeday'
+puts 'Exit'
+
 end
 end
 
