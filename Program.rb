eb040a209b124e3a9c9d4235fb570593aca3e626
domenicode <domenico@christophervigliotti.com>
2016-10-09 16:28:06 -0400

    made simpler

--- Program.rb
+++ Program.rb
@@ -1,26 +1,27 @@
 x=1
 puts 'use Help to see a list of commands'
 while x == 1
-command = gets.chomp
-if command == 'WhatsYourName'
+command = gets.chomp.downcase
+if command == 'whatsyourname'
 puts 'My Name is Joe'
-elsif command == 'HowAreYou'
+elsif command == 'howareyou'
 puts '| |'
 puts '_  _'
 puts ' -- '
-elsif command == 'WhatDayIsIt'
+elsif command == 'whatdayisit'
 puts 'Today is ' + Time.now.strftime("%A")
-elsif command == 'WhatTimeIsIt'
+elsif command == 'whattimeisit'
 puts 'It is ' + Time.now.strftime("%I : %M %p %Z")
-elsif command == 'Exit'
+elsif command == 'exit'
 output= x=2
-elsif command == 'Help'
+elsif command == 'help'
 puts 'Enter One of these:'
 puts 'WhatsYourName'
 puts 'HowAreYou'
 puts 'WhatDayIsIt'
 puts 'WhatTimeIsIt'
 puts 'Exit'
+puts 'You can also make them lowercase'
 else
 puts 'Not Valid, Use Help, I Beg You'
 end
